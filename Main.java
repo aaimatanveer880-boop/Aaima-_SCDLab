244f475cc40b1aec2d13c217a418b62d859d081d
aaima tanveer <aaima@gmail.com>
2026-03-16 09:45:34 +0500

    Student Details

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Students s1 = new Students();
+
+        s1.name = "Aaima";
+        s1.rollNumber = 101;
+
+        Students s2 = new Students();
+
+        s2.name = "Eman";
+        s2.rollNumber = 102;
+
+        s1.displayInfo();
+        s2.displayInfo();
+       
+    }
+}
---
+++ Students.java
@@ -0,0 +1,12 @@
+public class Students {
+    
+    String name;
+    int rollNumber;
+
+    public void displayInfo(){
+        System.out.println("Student name is=" + name);
+        System.out.println("Roll No is=" +  rollNumber);
+
+        System.out.println("....................................");
+    }
+}
