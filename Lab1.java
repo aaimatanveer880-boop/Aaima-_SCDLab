5dcf84ae8b584171789fc1c3b6ec0424dc7e5dbd
aaima tanveer <aaima@gmail.com>
2026-03-24 22:57:09 +0500

    My first program

--- Lab1.java
+++ Lab1.java
@@ -1,8 +1,5 @@
-public class Lab1 {public static void main(String[] args){
-<<<<<<< HEAD
-System.out.println("This is the first program of AAIMA TANVEER ");
-=======
+public class Lab1 
+    {public static void main(String[] args){
 System.out.println("This is the first program of AAIMA TANVEER");
 }
->>>>>>> b2d83af2cde48222f0eb0112ddf69e414cbde459
 }
