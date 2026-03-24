baf6a251a41e65070e61f5652c9d71e71b0d4c1f
aaima tanveer <aaima@gmail.com>
2026-03-24 22:31:58 +0500

    Added to files

--- Lab1.java
+++ Lab1.java
@@ -1,4 +1,4 @@
 public class Lab1 {public static void main(String[] args){
-System.out.println("This is the first program of Aaima");
+System.out.println("This is the first program of AAIMA TANVEER ");
 }
 }
