a60d1ae147b116ebb0d79b9b456d614184cdff27
aaima tanveer <aaima@gmail.com>
2026-03-30 13:22:37 +0500

    BankAccount Labtask

--- Main.java
+++ Main.java
@@ -1,18 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        Students s1 = new Students();
+        BankAccount account1 = new BankAccount("Aaima", 0.0);
+        BankAccount account2 = new BankAccount("Eman", 1000.0);
 
-        s1.name = "Aaima";
-        s1.rollNumber = 101;
+        System.out.println("\n Account 2 Initial Transactions ");
+        account2.deposit(2000);
+        account2.withdraw(200);
+        account2.displayAccountInfo();
 
-        Students s2 = new Students();
+        System.out.println("\n Account 1 Initial Transactions ");
+        account1.withdraw(-100);
+        account1.withdraw(50);
+        account1.displayAccountInfo();
 
-        s2.name = "Eman";
-        s2.rollNumber = 102;
+        System.out.println("\n Account 1 Final Info ");
+        System.out.println("Account Holder: " + account1.getAccountHolder());
+        System.out.println("Final Balance: " + account1.getBalance());
 
-        s1.displayInfo();
-        s2.displayInfo();
-       
+        System.out.println("\n Account 2 Final Info ");
+        System.out.println("Account Holder: " + account2.getAccountHolder());
+        System.out.println("Final Balance: " + account2.getBalance());
     }
 }
